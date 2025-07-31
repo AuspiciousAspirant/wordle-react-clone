f7784a0df942b96120ed65f1fdc1a6effafa83d9
AuspiciousAspirant <162094590+AuspiciousAspirant@users.noreply.github.com>
2025-07-31 09:24:49 -0400

    Vitest installed & clean up

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
+/// <reference types='vitest' />
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import type { UserConfigExport } from 'vite'
+import { configDefaults } from 'vitest/config' // ✅ Important
+import { type UserConfig as VitestUserConfig } from 'vitest/config' // ✅ Optional for TS types
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    tailwindcss()
   ],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+} as UserConfigExport)
